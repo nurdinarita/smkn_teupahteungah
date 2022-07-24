3b75afca028ec0f861b7357d408dc25189c0d4ca
nurdinarita <nurdinarita23@gmail.com>
2022-07-24 13:24:03 +0700

    penambahan icon

--- resources/views/pages/layouts/main.blade.php
+++ resources/views/pages/layouts/main.blade.php
@@ -3,6 +3,7 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <link rel="shortcut icon" href="{{ url('favicon.ico') }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
